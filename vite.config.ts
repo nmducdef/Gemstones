3785e7da704318a9e997adc1a136f3f66e0b6260
mducdef <nguyenminhduc0999@gmail.com>
2026-02-08 03:15:26 +0700

    m

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://123.31.17.35:8099',
+        changeOrigin: true
+      }
+    }
   },
   css: {
     devSourcemap: true
